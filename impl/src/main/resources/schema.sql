ef850a89ba55980e569cd7577869f64412339c1d
Timofey Cheberuk <timofey13052005@gmail.com>
2025-02-11 10:16:42 +0300

    deleting rubbish

--- impl/src/main/resources/schema.sql
+++ impl/src/main/resources/schema.sql
@@ -1,35 +1,35 @@
 create table if not exists Taco_Order (
-                                          id bigint not null auto_increment primary key,
-                                          delivery_Name varchar(50) not null,
-                                          delivery_Street varchar(50) not null,
-                                          delivery_City varchar(50) not null,
-                                          delivery_State varchar(2) not null,
-                                          delivery_Zip varchar(10) not null,
-                                          cc_number varchar(16) not null,
-                                          cc_expiration varchar(5) not null,
-                                          cc_cvv varchar(3) not null,
-                                          placed_at datetime not null
+    id bigint not null auto_increment primary key,
+    delivery_Name varchar(50) not null,
+    delivery_Street varchar(50) not null,
+    delivery_City varchar(50) not null,
+    delivery_State varchar(2) not null,
+    delivery_Zip varchar(10) not null,
+    cc_number varchar(16) not null,
+    cc_expiration varchar(5) not null,
+    cc_cvv varchar(3) not null,
+    placed_at datetime not null
 );
 
 create table if not exists Taco (
-                                    id bigint not null auto_increment primary key,
-                                    name varchar(50) not null,
-                                    taco_order bigint not null,
-                                    taco_order_key bigint not null,
-                                    created_at datetime not null
+    id bigint not null auto_increment primary key,
+    name varchar(50) not null,
+    taco_order bigint not null,
+    taco_order_key bigint not null,
+    created_at datetime not null
 );
 
 create table if not exists Ingredient_Ref (
-                                              ingredient varchar(4) not null,
-                                              taco bigint not null,
-                                              taco_key bigint not null,
-                                              primary key (ingredient, taco)
+    ingredient varchar(4) not null,
+    taco bigint not null,
+    taco_key bigint not null,
+    primary key (ingredient, taco)
 );
 
 create table if not exists Ingredient (
-                                          id varchar(4) not null primary key,
-                                          name varchar(25) not null,
-                                          type varchar(10) not null
+    id varchar(4) not null primary key,
+    name varchar(25) not null,
+    type varchar(10) not null
 );
 
 alter table Taco
